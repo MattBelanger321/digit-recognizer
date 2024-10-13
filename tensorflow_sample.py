5650b3564234399f3ca3f491dcdad7d682e77ac5
Matthew Belanger <matt_belanger1@outlook.com>
2024-10-12 22:06:19 -0400

    normalizing the input data

--- tensorflow_sample.py
+++ tensorflow_sample.py
@@ -2,7 +2,40 @@ import tensorflow as tf
 
 import data_parser as parser
 
-(images, labels) = parser.parse_mnist_data()
-print(images[0].shape)
+from numpy import mean
+from numpy import std
+from matplotlib import pyplot as plt
+from sklearn.model_selection import KFold
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.optimizers import SGD
+
+# Normalize 0-1 a single image
+def normalize(image, scale=255.0):
+	# convert from integers to floats
+	image_norm = image.astype('float32')
+	# normalize to range 0-1
+	image_norm = image_norm / scale
+	# return normalized images
+	return image_norm
+
+# normailze the entire training data
+def normalize_images(images_raw):
+	for i in range(images_raw.shape[0]):
+		images.append( normalize(images_raw[i]) )
+	return images
+
+(images_raw, labels) = parser.parse_mnist_data()
+images = normalize_images(images_raw)
+
+
+
+
+
+
 
 
